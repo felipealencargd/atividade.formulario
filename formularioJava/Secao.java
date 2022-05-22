9abfdf292446984ce429b24542e8ae246166a24e
felipealencargd <felipealencarpb@gmail.com>
2022-05-22 17:28:25 -0300

    Create Secao.java

---
+++ formularioJava/Secao.java
@@ -0,0 +1,9 @@
+package aps.src.padroesDeProjeto.formularioJava;
+
+public class Secao extends ItemFormComposto{
+
+    public Secao(String titulo, String subtitulo, String descricao) {
+        super(titulo, subtitulo, descricao);
+    }
+
+}
