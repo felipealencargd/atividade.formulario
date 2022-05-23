62b169e61a11a6b1d5cf72d39032b5269ed4c5bc
felipealencargd <felipealencarpb@gmail.com>
2022-05-23 01:52:25 -0300

    Create ValidadorRespostaTextual.java

---
+++ formularioJava/ValidadorRespostaTextual.java
@@ -0,0 +1,32 @@
+package aps.src.padroesDeProjeto.formularioJava;
+
+public class ValidadorRespostaTextual implements ValidadorResposta{
+
+    private int tamMax;
+    private int tamMin;
+
+    @Override
+    public void validarResposta(String resposta) {
+        if (resposta.length() >= this.getTamMin() && resposta.length() <= this.getTamMax()){
+            System.out.println("Resposta válida");
+        } else {
+            System.err.println("Resposta inválida");
+        }
+    }
+
+    public int getTamMax() {
+        return tamMax;
+    }
+
+    public void setTamMax(int tamMax) {
+        this.tamMax = tamMax;
+    }
+
+    public int getTamMin() {
+        return tamMin;
+    }
+
+    public void setTamMin(int tamMin) {
+        this.tamMin = tamMin;
+    }
+}
